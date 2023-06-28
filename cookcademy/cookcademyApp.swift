e332ecd9b603e08506ad0f2f01a365ab1c669768
Donald Onyino <58218526+porkytheblack@users.noreply.github.com>
2023-06-28 12:44:02 +0300

    Initial Commit

---
+++ cookcademy/cookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookcademyApp.swift
+//  cookcademy
+//
+//  Created by Donald Onyino on 28/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct cookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
